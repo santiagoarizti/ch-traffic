b290f7f932bda716fa2aa51f2e17acebc86f0695
Santiago <santiago@cyberpuerta.mx>
2022-01-27 13:35:09 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="app-header">
+    <img src="@/assets/logo.svg" class="app-header-logo">
+    <nav class="app-header-nav">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/game">Game</RouterLink>
+    </nav>
+  </header>
+  <section class="app-body">
+    <RouterView />
+  </section>
+
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+body {
+  min-height: 100vh;
+  color: var(--color-text);
+  background: var(--color-background);
+  transition: color 0.5s, background-color 0.5s;
+  line-height: 1.6;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+    Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  font-size: 1rem;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 2rem;
+
+  font-weight: normal;
+}
+
+.app-header {
+  line-height: 1.5;
+  max-height: 100vh;
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+}
+
+.app-header-logo {
+  height: 30px;;
+}
+
+.app-header-nav {
+  width: 100%;
+  font-size: 12px;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+</style>
---
+++ src/views/GameView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+The game
+</template>
+
+<style>
+</style>
